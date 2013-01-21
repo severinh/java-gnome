04d305ef46d5a8c538bc3771af67083aef87e07d
Severin Heiniger <severinheiniger@gmail.com>
2013-01-21 19:56:11 +0100

    Fix include directives.

--- src/bindings/org/gnome/vala/ValaSourceLocationOverride.c
+++ src/bindings/org/gnome/vala/ValaSourceLocationOverride.c
@@ -33,7 +33,7 @@
 
 #include <jni.h>
 #include <gtk/gtk.h>
-#include <vala-2.0/vala.h>
+#include <vala-0.20/vala.h>
 #include "bindings_java.h"
 #include "org_gnome_vala_ValaSourceLocationOverride.h"
 
